be0f6e5b327d6169ad1ec8ba4dd0368dd9abab0b
Ansh Sharma <sansh2405@gmail.com>
2026-01-20 20:56:58 +0530

    made basic routes folder structure

--- app.js
+++ app.js
@@ -2,21 +2,14 @@ const express = require("express");
 const morgan = require("morgan");
 
 const errorHandler = require("./middleware/errorHandler");
+const healthRoutes = require("./routes/health.routes");
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.get("/health", (req, res) => {
-  res.status(200).json({ status: "OK" });
-});
-
-app.get("/error-test", (req, res, next) => {
-  const err = new Error("Test error");
-  err.statusCode = 400;
-  next(err);
-});
+app.use(healthRoutes);
 
 app.use(errorHandler);
 
