09edc57063473293f13809b48e16cd856ff5ff2e
Ansh Sharma <sansh2405@gmail.com>
2026-01-20 21:24:16 +0530

    added mongodb atlas connection setup

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const dotenv = require("dotenv");
 const app = require("./app");
+const connectDB = require("./config/db");
 
 dotenv.config();
+
+connectDB();
 
 const PORT = process.env.PORT || 3000;
 
