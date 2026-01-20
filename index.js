37a567620afc2dd92e6ec5b8a83dbf566edf7e53
Ansh Sharma <sansh2405@gmail.com>
2026-01-20 20:41:45 +0530

    added request logging middleware

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 
 dotenv.config();
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan("dev"));
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "OK" });
