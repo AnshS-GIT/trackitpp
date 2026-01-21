59172a56cc56e14b9da0d4fd036f3fe893ac181f
Ansh Sharma <sansh2405@gmail.com>
2026-01-21 20:02:15 +0530

    added login service for user authentication

--- services/user.service.js
+++ services/user.service.js
@@ -18,6 +18,32 @@ const createUser = async ({ name, email, password, role }) => {
   return user;
 };
 
+const loginUser = async ({ email, password }) => {
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    const error = new Error("Invalid email or password");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  if (!user.isActive) {
+    const error = new Error("User account is inactive");
+    error.statusCode = 403;
+    throw error;
+  }
+
+  const isMatch = await user.comparePassword(password);
+  if (!isMatch) {
+    const error = new Error("Invalid email or password");
+    error.statusCode = 401;
+    throw error;
+  }
+
+  return user;
+};
+
 module.exports = {
   createUser,
+  loginUser,
 };
