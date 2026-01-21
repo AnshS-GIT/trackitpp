b8ac5e8fd53b5c3ada08dd571b6e474454e4df92
Ansh Sharma <sansh2405@gmail.com>
2026-01-21 16:32:18 +0530

    added password hashing and comparison to user model

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
 const baseOptions = require("./base.model");
 
 const userSchema = new mongoose.Schema(
@@ -36,5 +37,18 @@ const userSchema = new mongoose.Schema(
   },
   baseOptions
 );
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+
+  next();
+});
+
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model("User", userSchema);
